36d08f53f1ae53f00f8e2729b2929f55c34e735a
unknown <moath.malkawi@techconsulting.tech>
2025-12-12 00:16:48 -0500

    Remove ssh_public_key from root module

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -39,10 +39,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     azurerm_network_interface.nic.id
   ]
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
-  }
+  
 
   os_disk {
     caching              = "ReadWrite"
